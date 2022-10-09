69ed7690e8ed029650d475e32400e489c5a492e6
Akshay Kasary <Akshay.Kasary@ceridian.com>
2022-10-09 20:40:57 +0400

    Revert "new changes"
    This reverts commit b2ca3cc80049fd72bb2ace57861ddf4137b89f03.

---
+++ customStack/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace customStack
+{
+    public class CustomStack <T>
+    {
+        public static T[] array = new T[10];
+
+        public static int index=0;
+
+        public CustomStack() { }
+
+        public void Push(T a)
+        {
+            index++;
+            array[index] = a;
+
+        }
+        public T Pop()
+        {
+            index--;
+            
+                var a = array[index + 1];
+                return a;
+            
+
+        }
+
+        public int Count()
+        {
+            return index+1;
+        }
+
+      
+    }
+}
